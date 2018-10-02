30755742ab448352a65741a73b5bc2a4efd33063
Madhu Sudan Soni <Madhu003>
2018-10-02 06:13:28 +0530

    => change in controller of menuList!!

--- src/main/java/restaurent/controller/MenuItemController.java
+++ src/main/java/restaurent/controller/MenuItemController.java
@@ -1,14 +1,14 @@
 package restaurent.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController	
 @RequestMapping("/rest/menu_item")
 public class MenuItemController {
 	
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	@GetMapping("/list")
 	public String getList(){
 		
 		return "String Got!!";
