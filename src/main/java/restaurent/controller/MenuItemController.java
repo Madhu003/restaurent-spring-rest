0201d8b32aac57196549398ffdbbb8d556485a2a
Madhu Sudan <Madhu003>
2018-10-04 09:27:23 +0530

    => first mock api created of menuitem!! => som changes happened in web.xml, element tag added for tomcat error problem!! => servlet version changed in pom.xml at build tag!!

--- src/main/java/restaurent/controller/MenuItemController.java
+++ src/main/java/restaurent/controller/MenuItemController.java
@@ -4,13 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import restaurent.entity.MenuItem;
+
 @RestController	
-@RequestMapping("/rest/menu_item")
+@RequestMapping("/menu_item")
 public class MenuItemController {
 	
-	@GetMapping("/list")
-	public String getList(){
-		
-		return "String Got!!";
+	@GetMapping
+	public MenuItem getList(){
+		return new MenuItem("madhu", "sudan");
 	}
 }
--- src/main/java/restaurent/entity/MenuItem.java
+++ src/main/java/restaurent/entity/MenuItem.java
@@ -1,5 +1,26 @@
 package restaurent.entity;
 
 public class MenuItem {
-
+	String name;
+	String imageUrl;
+	
+	public MenuItem(String name, String imageUrl) {
+		super();
+		this.name = name;
+		this.imageUrl = imageUrl;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	
 }
