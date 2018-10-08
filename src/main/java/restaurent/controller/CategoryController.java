12f4009d3e9cd588bbe05a6b49d954aebba00406
Madhu Sudan <Madhu003>
2018-10-08 09:11:49 +0530

    Hibernate config implemented
    => @configuration hibernate connection pool c3p0 added!!
    => transacrion manager added!!

--- src/main/java/restaurent/controller/CategoryController.java
+++ src/main/java/restaurent/controller/CategoryController.java
@@ -2,6 +2,7 @@ package restaurent.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,7 @@ import restaurent.service.CategoryService;
 @RequestMapping("/category")
 public class CategoryController {
 
-	// @Autowired
+	@Autowired
 	private CategoryService categoryService;
 
 	@GetMapping
